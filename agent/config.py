1dcbc51149ff35681c461798048c6593667d3ff9
hant0505 <23020532@vnu.edu.vn>
2026-05-28 18:16:03 +0700

    fix config

--- agent/config.py
+++ agent/config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class GroqLLMManager: # Đổi tên class cho đúng bản chất
+class OpenRouterLLMManager: # Đổi tên class cho đúng bản chất
     def __init__(self):
         # Đọc danh sách API Key của Groq từ file .env
-        keys = os.getenv("GROQ_API_KEY") 
+        keys = os.getenv("OPENROUTER_API_KEY") 
 
         if not keys:
-            raise ValueError("GROQ_API_KEY not found!")
+            raise ValueError("OPENROUTER_API_KEY not found!")
 
         self.api_keys = [
             k.strip() for k in keys.split(",")
@@ -23,17 +23,18 @@ class GroqLLMManager: # Đổi tên class cho đúng bản chất
     def get_llm(self):
         key = self.api_keys[self.index]
         self.index = (self.index + 1) % len(self.api_keys)
+        MODEL_NAME = "google/gemini-2.0-flash-lite-001"
 
-    
-        return LLM(
-            model="groq/llama-3.3-70b-specdec", 
-            api_key=key,
+        return ChatOpenAI(
+            model=MODEL_NAME,
+            openai_api_key=key,
+            openai_api_base="https://openrouter.ai/api/v1",
             temperature=0.1
         )
 
 
 # Singleton
-llm_manager = GroqLLMManager()
+llm_manager = OpenRouterLLMManager()
 
 
 def get_llm():
