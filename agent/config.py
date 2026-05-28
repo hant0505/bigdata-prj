04d05d5b8a53d7f01f760597927754bbcaf6dbd8
hant0505 <23020532@vnu.edu.vn>
2026-05-28 18:34:00 +0700

    change Openrouter

--- agent/config.py
+++ agent/config.py
@@ -1,14 +1,12 @@
 import os
-import itertools
-from crewai import LLM
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-class OpenRouterLLMManager: # Đổi tên class cho đúng bản chất
+class OpenRouterLLMManager:
     def __init__(self):
-        # Đọc danh sách API Key của Groq từ file .env
-        keys = os.getenv("OPENROUTER_API_KEY") 
+        keys = os.getenv("OPENROUTER_API_KEY")
 
         if not keys:
             raise ValueError("OPENROUTER_API_KEY not found!")
@@ -23,19 +21,18 @@ class OpenRouterLLMManager: # Đổi tên class cho đúng bản chất
     def get_llm(self):
         key = self.api_keys[self.index]
         self.index = (self.index + 1) % len(self.api_keys)
+
         MODEL_NAME = "google/gemini-2.0-flash-lite-001"
 
         return ChatOpenAI(
             model=MODEL_NAME,
-            openai_api_key=key,
-            openai_api_base="https://openrouter.ai/api/v1",
+            api_key=key,
+            base_url="https://openrouter.ai/api/v1",
             temperature=0.1
         )
 
 
-# Singleton
 llm_manager = OpenRouterLLMManager()
-
 
 def get_llm():
     return llm_manager.get_llm()
